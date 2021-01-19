641ccaa56db559e717bf7ae46a0ee98426902f75
Therese <64402455+Thessan@users.noreply.github.com>
2021-01-19 09:13:37 +0100

    cleared code for template

--- frontend/src/components/GoBack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const GoBack = () => {
-    
-    return (
-        <p>Don't have an account? <Link to={`/`}>Sign up here</Link></p>
-    )
-}
--- frontend/src/components/LoginHere.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const LoginHere = () => {
-    return (
-        <p>Already a user? <Link to={`/sessions`}>Login here</Link></p>
-    )
-}
